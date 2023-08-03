991f73bec4c0b19b79913c8415fa0fe6c40f56eb
Hanif <yahyafaikar17@gmail.com>
2023-08-03 19:11:52 +0700

    add get by id

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -10,12 +10,7 @@ import (
 
 func GetMovies(res http.ResponseWriter, req *http.Request) {
 	if req.Method != "GET" {
-		message := []byte(`{
-			"success": false,
-			"message": "Invalid HTTP method executed",
-		   }`)
-
-		utils.ReturnJsonResponse(res, http.StatusMethodNotAllowed, message)
+		validateHttpMethod(res)
 		return
 	}
 
@@ -29,17 +24,69 @@ func GetMovies(res http.ResponseWriter, req *http.Request) {
 	movieJSON, err := json.Marshal(map[string]interface{}{
 		"movieList": movies,
 	})
-	if err != nil {
-		// Add the response return message
-		HandlerMessage := []byte(`{
-	  "success": false,
-	  "message": "Error parsing the movie data",
-	 }`)
 
-		utils.ReturnJsonResponse(res, http.StatusInternalServerError, HandlerMessage)
+	if err != nil {
+		message, _ := json.Marshal(models.ErrorMsg{
+			Success: false,
+			Message: "Failed parse movie data",
+		})
+
+		utils.ReturnJsonResponse(res, http.StatusInternalServerError, message)
 		return
 	}
 
 	utils.ReturnJsonResponse(res, http.StatusOK, movieJSON)
+}
 
+func GetMovieById(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		validateHttpMethod(res)
+		return
+	}
+
+	var id, ok = req.URL.Query()["id"]
+	if !ok {
+		message, _ := json.Marshal(models.ErrorMsg{
+			Success: false,
+			Message: "Required request ID",
+		})
+		utils.ReturnJsonResponse(res, http.StatusInternalServerError, message)
+		return
+	}
+
+	var movie, found = db.Movies[id[0]]
+	if !found {
+		message, _ := json.Marshal(models.ErrorMsg{
+			Success: false,
+			Message: "ID not found : " + id[0],
+		})
+		utils.ReturnJsonResponse(res, http.StatusInternalServerError, message)
+		return
+	}
+
+	movieJson, err := json.Marshal(&movie)
+	if err != nil {
+		failedParseData(res)
+		return
+	}
+
+	utils.ReturnJsonResponse(res, http.StatusOK, movieJson)
+}
+
+func validateHttpMethod(res http.ResponseWriter) {
+	message, _ := json.Marshal(models.ErrorMsg{
+		Success: false,
+		Message: "Invalid HTTP Method",
+	})
+
+	utils.ReturnJsonResponse(res, http.StatusMethodNotAllowed, message)
+}
+
+func failedParseData(res http.ResponseWriter) {
+	message, _ := json.Marshal(models.ErrorMsg{
+		Success: false,
+		Message: "Failed parse movie data",
+	})
+
+	utils.ReturnJsonResponse(res, http.StatusInternalServerError, message)
 }
--- main.go
+++ main.go
@@ -17,9 +17,10 @@ func main() {
 
 	// get movies
 	http.HandleFunc("/movies", handlers.GetMovies)
+	http.HandleFunc("/movie", handlers.GetMovieById)
 
 	err := http.ListenAndServe(":3000", nil)
-	// print any server-based error messages
+
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
